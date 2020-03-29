895594ed655519384cc41ff752495f3a1241eace
AnatolIgnatiev <61428823+AnatolIgnatiev@users.noreply.github.com>
2020-03-29 16:18:22 +0300

    HomeWork#7 Fixed

--- HomeWork#7/Rgistry/Program.cs
+++ HomeWork#7/Rgistry/Program.cs
@@ -21,7 +21,33 @@ namespace Registry
             for (int i = 0; i < numberOfStudents; i++)
             {
                 Student student = new Student();
+                DateTime inputDate = new DateTime();
+                Console.WriteLine("Enter Date and time of students admission (dd.mm.yyyy h:mm:ss) or enter empty string if student is admiting now: ");
+                string date = Console.ReadLine();
+                if (!string.IsNullOrEmpty(date) || DateTime.TryParse(date, out inputDate))
+                {
+                    student.DateOfAdmission = inputDate;
+                }
+                else
+                {
+                    student.DateOfAdmission = DateTime.Now;
+                }
+
+                Console.WriteLine("Enter new students name: ");
+                student.Name = Console.ReadLine();
+
+                Console.WriteLine("Enter new students surname: ");
+                student.LastName = Console.ReadLine();
+
+                Console.WriteLine("Enter students adress in next format: country,city,street,house,appartment");
+                string[] inputAdress = Console.ReadLine().Split(',');
+                student.Address.country = inputAdress[0];
+                student.Address.city = inputAdress[1];
+                student.Address.address = inputAdress[2] + "," + inputAdress[3] + "," + inputAdress[4];
+                Console.WriteLine();
+
                 StudentRegistry.AddStudent(student);
+
                 Console.WriteLine(student.GetStudentInfo());
             }
 
