e7e8d039c4fbe38fd7552c222ce50100e6c7829d
AnatolIgnatiev <61428823+AnatolIgnatiev@users.noreply.github.com>
2020-05-22 01:35:23 +0300

    HomeWork3(Delegates)

---
+++ HomeWork3(Delegates)/HomeWork3(Delegates)/AlphaNumbericCollector.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork3_Delegates_
+{
+    class AlphaNumbericCollector : ICollector
+    {
+        private List<string> numbericStringsCollector { get; set; } = new List<string>();
+        private bool Validate(string inputString)
+        {
+            return inputString.Any(symbol => char.IsDigit(symbol));
+        }
+        public void Save(string inputString)
+        {
+            if (Validate(inputString))
+            {
+                numbericStringsCollector.Add(inputString);
+            }
+        }
+        public List<string> GetSaved()
+        {
+            return new List<string>(numbericStringsCollector);
+        }
+        public IEnumerator<string> GetEnumerator()
+        {
+            return numbericStringsCollector.GetEnumerator();
+        }
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return ((IEnumerable)this).GetEnumerator();
+        }
+    }
+}
---
+++ HomeWork3(Delegates)/HomeWork3(Delegates)/StringCollector.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork3_Delegates_
+{
+    class StringCollector : ICollector
+    {
+        private List<string> stringCollector { get; set; } = new List<string>();
+        private bool Validate(string inputString)
+        {
+            return inputString.All(symbol => !char.IsDigit(symbol));            
+        }
+        public void Save(string inputString)
+        {
+            if (Validate(inputString))
+            {
+                stringCollector.Add(inputString);
+            }
+        }
+        public List<string> GetSaved()
+        {
+            return new List<string>(stringCollector);
+        }
+        public IEnumerator<string> GetEnumerator()
+        {
+            return stringCollector.GetEnumerator();
+        }
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return ((IEnumerable)this).GetEnumerator();
+        }
+    }
+}
